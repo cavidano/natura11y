bbcc003a13f980dac437a927ce7e6265e66f749b
Carl Avidano <cavidano@gmail.com>
2021-10-19 12:59:11 -0400

    added nav--icons modifier

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,7 +6,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const devDir = './dist/html';
-const devIndex = 'grid.html';
+const devIndex = 'buttons.html';
 
 module.exports = merge(common, {
     mode: 'development',
