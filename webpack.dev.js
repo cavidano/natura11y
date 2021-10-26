bebdcd48f11747eef30c3df774facbc783834c62
Carl Avidano <cavidano@gmail.com>
2021-10-26 04:16:29 -0400

    adding form validation example

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,7 +6,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const devDir = './dist/html';
-const devIndex = 'form-validation.html';
+const devIndex = 'forms.html';
 
 module.exports = merge(common, {
     mode: 'development',
