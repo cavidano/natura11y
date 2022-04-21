4ab0f30f4fe27eb49d14c48b87e18c6b36fc9228
Carl Avidano <cavidano@gmail.com>
2022-04-21 02:36:04 -0400

    cleaning up landing page examples

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,20 +5,20 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// const devDir = './dist/html';
-// const devDir = './dist/html/comps/landing';
-const devDir = './dist/html/comps/bird-landing-page';
+const devDir = './dist/html';
+// const devDir = './dist/html/comps/bird-landing-page';
 
 // const devIndex = 'buttons.html';
 // const devIndex = 'footers.html';
 // const devIndex = 'accordion.html';
+const devIndex = 'articles.html';
 // const devIndex = 'tables.html';
 // const devIndex = 'spacers.html';
 // const devIndex = 'buttons.html';
 // const devIndex = 'modal.html';
 // const devIndex = 'form-validation.html';
 // const devIndex = 'backdrops.html';
-const devIndex = 'index.html';
+// const devIndex = 'index.html';
 
 module.exports = merge(common, {
     mode: 'development',
