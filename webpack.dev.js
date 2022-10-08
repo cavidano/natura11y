34468a288f9dbe7d70a88196ebf028d141881006
Carl Avidano <cavidano@gmail.com>
2022-10-08 03:20:55 -0400

    update button group

--- webpack.dev.js
+++ webpack.dev.js
@@ -32,7 +32,7 @@ const devPages = {
 
 const devDir = './dist/html';
 
-const devPage = `${devPages.Modal}.html`;
+const devPage = `${devPages.Buttons}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
