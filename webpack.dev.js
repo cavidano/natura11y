bfd765e95f88dd732f8682645b746c4d1a97c1fb
Carl Avidano <cavidano@gmail.com>
2022-10-08 07:02:53 -0400

    added semicolon

--- webpack.dev.js
+++ webpack.dev.js
@@ -32,7 +32,7 @@ const devPages = {
 
 const devDir = './dist/html';
 
-const devPage = `${devPages.Buttons}.html`;
+const devPage = `${devPages.Modal}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
