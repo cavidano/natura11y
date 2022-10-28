e70a82b12a37abcc7be99a95537f433a21eebc0d
Carl Avidano <cavidano@gmail.com>
2022-10-28 16:25:37 -0400

    updated @use

--- src/index.js
+++ src/index.js
@@ -11,7 +11,38 @@ In this file:
 // A. SCSS
 //////////////////////////////////////////////
 
-import './scss/natura11y.scss';
+import './scss/accessibility.scss';
+import './scss/accordion.scss';
+import './scss/alert.scss';
+import './scss/article.scss';
+import './scss/aspect-ratio.scss';
+import './scss/backdrop.scss';
+import './scss/border.scss';
+import './scss/breakpoint.scss';
+import './scss/button.scss';
+import './scss/card.scss';
+import './scss/collapse.scss';
+import './scss/color.scss';
+import './scss/container.scss';
+import './scss/display.scss';
+import './scss/document.scss';
+import './scss/flex.scss';
+import './scss/form.scss';
+import './scss/grid.scss';
+import './scss/icon.scss';
+import './scss/link.scss';
+import './scss/modal.scss';
+import './scss/navigation.scss';
+import './scss/opacity.scss';
+import './scss/overflow.scss';
+import './scss/position.scss';
+import './scss/shadow.scss';
+import './scss/sizing.scss';
+import './scss/spacing.scss';
+import './scss/tab.scss';
+import './scss/table.scss';
+import './scss/typography.scss';
+import './scss/z-index.scss';
 
 //////////////////////////////////////////////
 // B. JS
--- webpack.dev.js
+++ webpack.dev.js
@@ -32,7 +32,7 @@ const devPages = {
 
 const devDir = './dist/html';
 
-const devPage = `${devPages.Navigation}.html`;
+const devPage = `${devPages.Typography}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
