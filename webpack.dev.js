1ac134d6bb974a99f1d0557c7e7a3af425c5f7aa
Carl Avidano <cavidano@gmail.com>
2022-10-29 09:09:47 -0400

    adding foundational layers

--- webpack.dev.js
+++ webpack.dev.js
@@ -33,7 +33,7 @@ const devPages = {
 
 const devDir = './dist/html';
 
-const devPage = `${devPages.Card}.html`;
+const devPage = `${devPages.Backdrop}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
