8d6e08952e439c9609bc838a009739e39f90cd4a
Carl Avidano <cavidano@gmail.com>
2022-11-10 07:12:34 -0500

    cleaned up color comments

--- webpack.dev.js
+++ webpack.dev.js
@@ -34,7 +34,7 @@ const devPages = {
 
 const devDir = './dist/html';
 
-const devPage = `${devPages.Grid}.html`;
+const devPage = `${devPages.Typography}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
