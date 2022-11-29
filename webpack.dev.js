3da0505565bd60f580569974804b3069468b95d1
Carl Avidano <cavidano@gmail.com>
2022-11-29 15:12:42 -0500

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -35,7 +35,7 @@ const devPages = {
 
 const devDir = './dist/html';
 
-const devPage = `${devPages.Typography}.html`;
+const devPage = `${devPages.Form}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
