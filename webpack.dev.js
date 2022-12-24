f36e5d4b8ebd723dc0b4058055b2e9a701800b95
Carl Avidano <cavidano@gmail.com>
2022-12-24 07:07:11 -0500

    cleaned up hovert-focus

--- webpack.dev.js
+++ webpack.dev.js
@@ -35,7 +35,7 @@ const devPages = {
 
 const devDir = './dist/html';
 
-const devPage = `${devPages.Grid}.html`;
+const devPage = `${devPages.Accordion}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
