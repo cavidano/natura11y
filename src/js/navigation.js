7a94cf27a0f11d859034c725d8b517505a56a510
Carl Avidano <cavidano@gmail.com>
2023-05-19 10:09:41 -0400

    Update navigation.js

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,3 +1,7 @@
+//////////////////////////////////////////////
+// Navigation
+//////////////////////////////////////////////
+
 export default class Navigation {
 
 	#dropdownButtonList = document.querySelectorAll('[data-toggle="dropdown"]');
@@ -28,65 +32,16 @@ export default class Navigation {
 			dropdownButton.setAttribute('aria-expanded', false);
 			dropdownButton.setAttribute('aria-haspopup', true);
 
-			const hoverable = dropdownButton.hasAttribute('data-dropdown-hover');
-			const eventType = hoverable ? 'mouseenter' : 'click';
-			let timeoutID;
-			let leaveTimeoutID;
-
-			// Handle event on the dropdown button
-			dropdownButton.addEventListener(eventType, (event) => {
+			// Handle click event on the dropdown button
+			dropdownButton.addEventListener('click', (event) => {
 				event.preventDefault();
-				clearTimeout(leaveTimeoutID); // Clear any existing leave timeouts
-				if (!dropdownMenu.classList.contains('shown')) { // Check if dropdown is not already visible
-					timeoutID = setTimeout(() => {
-						this.toggleDropdown(dropdownButton, dropdownMenu);
-					}, 500); // 500ms delay
-				}
-			});
-
-			// Handle focus event on the dropdown button for accessibility
-			dropdownButton.addEventListener('focus', (event) => {
 				this.toggleDropdown(dropdownButton, dropdownMenu);
 			});
-
-			// Handle focusout event on the dropdown item for accessibility
-			dropdownButtonParent.addEventListener('focusout', (event) => {
-				// Delay the closing action so we can check the newly focused element
-				setTimeout(() => {
-					// If no child element of the dropdown item is currently focused...
-					if (!dropdownButtonParent.contains(document.activeElement)) {
-						// Close the dropdown
-						this.closeDropdown(dropdownButton, dropdownMenu);
-					}
-				}, 0);
-			});
-
-			if (hoverable) {
-				// Handle mouseleave event on the dropdown button and its parent
-				dropdownButtonParent.addEventListener('mouseleave', (event) => {
-					event.preventDefault();
-					clearTimeout(timeoutID); // Clear any existing timeouts
-					leaveTimeoutID = setTimeout(() => {
-						// Only close the dropdown if mouse is not hovering over the parent li
-						if (!dropdownButtonParent.matches(':hover')) {
-							this.closeDropdown(dropdownButton, dropdownMenu);
-						}
-					}, 500); // 500ms delay
-				});
-
-				// Handle mouseenter event to cancel leave timeout
-				dropdownButtonParent.addEventListener('mouseenter', (event) => {
-					event.preventDefault();
-					clearTimeout(leaveTimeoutID); // Clear any existing leave timeouts
-				});
-			}
 
 			// Handle click event anywhere on the window
 			window.addEventListener('click', (event) => {
 				let dropdownButtonClick = dropdownButtonParent.contains(event.target);
 				if (!dropdownButtonClick) {
-					clearTimeout(timeoutID);
-					clearTimeout(leaveTimeoutID);
 					this.closeDropdown(dropdownButton, dropdownMenu);
 				}
 			});
