ba585ad0e49505a4cc6326497e6e11c2515de8f4
Carl Avidano <cavidano@gmail.com>
2023-05-19 10:49:51 -0400

    Update navigation.js

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,50 +1,66 @@
 //////////////////////////////////////////////
-// Navigation
+// Tab
 //////////////////////////////////////////////
 
 export default class Navigation {
 
-	#dropdownButtonList = document.querySelectorAll('[data-toggle="dropdown"]');
+    #dropdownButtonList = document.querySelectorAll('[data-toggle="dropdown"]');
 
-	// Helper methods
-	toggleDropdown(dropdownButton, dropdownMenu) {
-		dropdownMenu.classList.toggle('shown');
-		let expanded = dropdownButton.getAttribute('aria-expanded');
+    #toggleDropdown(dropdownButton, dropdownMenu) {
+        dropdownMenu.classList.toggle('shown');
+        let expanded = dropdownButton.getAttribute('aria-expanded');
 
-		if (expanded === 'true') {
-			dropdownButton.setAttribute('aria-expanded', false);
-		} else if (expanded === 'false') {
-			dropdownButton.setAttribute('aria-expanded', true);
-		}
-	}
+        if (expanded === 'true') {
+            dropdownButton.setAttribute('aria-expanded', 'false');
+        } else if (expanded === 'false') {
+            dropdownButton.setAttribute('aria-expanded', 'true');
+        }
+    }
 
-	closeDropdown(dropdownButton, dropdownMenu) {
-		dropdownMenu.classList.remove('shown');
-		dropdownButton.setAttribute('aria-expanded', false);
-	}
+    #closeDropdown(dropdownButton, dropdownMenu) {
+        dropdownMenu.classList.remove('shown');
+        dropdownButton.setAttribute('aria-expanded', 'false');
+    }
 
-	// Init method
-	init() {
-		this.#dropdownButtonList.forEach((dropdownButton) => {
-			let dropdownButtonParent = dropdownButton.closest('li');
-			let dropdownMenu = dropdownButton.nextElementSibling;
+    init() {
+	
+        // Check if there are dropdown buttons
+        if (!this.#dropdownButtonList) {
+            console.warn('No dropdown buttons found');
+            return;
+        }
 
-			dropdownButton.setAttribute('aria-expanded', false);
-			dropdownButton.setAttribute('aria-haspopup', true);
+        // Single click listener for window
+        window.addEventListener('click', (event) => {
+            this.#dropdownButtonList.forEach((dropdownButton) => {
+                let dropdownButtonParent = dropdownButton.closest('li');
+                let dropdownMenu = dropdownButton.nextElementSibling;
 
-			// Handle click event on the dropdown button
-			dropdownButton.addEventListener('click', (event) => {
-				event.preventDefault();
-				this.toggleDropdown(dropdownButton, dropdownMenu);
-			});
+                let dropdownButtonClick = dropdownButtonParent.contains(event.target);
+                if (!dropdownButtonClick) {
+                    this.#closeDropdown(dropdownButton, dropdownMenu);
+                }
+            });
+        });
 
-			// Handle click event anywhere on the window
-			window.addEventListener('click', (event) => {
-				let dropdownButtonClick = dropdownButtonParent.contains(event.target);
-				if (!dropdownButtonClick) {
-					this.closeDropdown(dropdownButton, dropdownMenu);
-				}
-			});
-		});
-	}
+        this.#dropdownButtonList.forEach((dropdownButton) => {
+            let dropdownButtonParent = dropdownButton.closest('li');
+            let dropdownMenu = dropdownButton.nextElementSibling;
+
+            // Check if dropdown button has corresponding dropdown menu
+            if (!dropdownMenu) {
+                console.warn(`No dropdown menu found for dropdown button ${dropdownButton}`);
+                return;
+            }
+
+            dropdownButton.setAttribute('aria-expanded', 'false');
+            dropdownButton.setAttribute('aria-haspopup', 'true');
+
+            // Handle click event on the dropdown button
+            dropdownButton.addEventListener('click', (event) => {
+                event.preventDefault();
+                this.#toggleDropdown(dropdownButton, dropdownMenu);
+            });
+        });
+    }
 }
