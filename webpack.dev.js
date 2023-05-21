93c39cf8461ff6787baaed5dc24105af498309ba
Carl Avidano <cavidano@gmail.com>
2023-05-21 06:35:58 -0400

    cleaned up grid component

--- webpack.dev.js
+++ webpack.dev.js
@@ -36,7 +36,7 @@ const devPages = {
 
 const devDir = './dist/html';
 
-const devPage = `${devPages.Form}.html`;
+const devPage = `${devPages.Grid}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
