f160516aeb58c662e0d64f72b80c2babcce075eb
Carl Avidano <cavidano@gmail.com>
2023-08-01 12:17:30 -0400

    test backdrop

--- webpack.dev.js
+++ webpack.dev.js
@@ -35,7 +35,7 @@ const devPages = {
 
 const devDir = './dist/html';
 
-const devPage = `${devPages.Lightbox}.html`;
+const devPage = `${devPages.Backdrop}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
