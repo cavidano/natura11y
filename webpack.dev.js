3ae7da29921baeb9e434d1ecdb7c6751839c7762
Carl Avidano <cavidano@gmail.com>
2023-11-25 10:58:58 -0500

    adding border-radius properties

--- webpack.dev.js
+++ webpack.dev.js
@@ -34,7 +34,7 @@ const devPages = {
 
 const devDir = './dist/html';
 
-const devPage = `${devPages.Typography}.html`;
+const devPage = `${devPages.Border}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
