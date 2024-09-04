ba223fac053d5c591b2d22c05cea3e4ddfe606e9
Carl Avidano <cavidano@gmail.com>
2024-09-03 20:44:13 -0400

    cleaning up skip links

--- webpack.dev.js
+++ webpack.dev.js
@@ -39,7 +39,7 @@ const devPages = {
 }
 
 const devDir = './dist/html';
-const devPage = `${devPages.Track}.html`;
+const devPage = `${devPages.New}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
