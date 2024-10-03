d25100113acc67f3e676886d618933189d4d0026
Carl Avidano <cavidano@gmail.com>
2024-10-03 04:08:38 -0400

    gradient scss clean up

--- webpack.dev.js
+++ webpack.dev.js
@@ -38,7 +38,7 @@ const devPages = {
 }
 
 const devDir = './dist/html';
-const devPage = `${devPages.Form}.html`;
+const devPage = `${devPages.Card}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
