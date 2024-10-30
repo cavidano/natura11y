cb78eebfb2cd9cdbb31358d3fecaf77d1eb97f98
Carl Avidano <cavidano@gmail.com>
2024-10-30 12:13:21 -0400

    update track

--- webpack.dev.js
+++ webpack.dev.js
@@ -38,7 +38,7 @@ const devPages = {
 }
 
 const devDir = './dist/html';
-const devPage = `${devPages.Backdrop}.html`;
+const devPage = `${devPages.Track}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
