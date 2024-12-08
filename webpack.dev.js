f17e732f2a0e16c018b4194f56bd77f045d85120
Carl Avidano <cavidano@gmail.com>
2024-12-08 04:41:45 -0500

    removed 'link--has-icon' selector

--- webpack.dev.js
+++ webpack.dev.js
@@ -38,7 +38,7 @@ const devPages = {
 }
 
 const devDir = './dist/html';
-const devPage = `${devPages.Track}.html`;
+const devPage = `${devPages.Megamenu}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
