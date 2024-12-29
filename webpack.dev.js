ea4f3d4903a306def5f12ef2804f560458cd3988
Carl Avidano <cavidano@gmail.com>
2024-12-29 06:47:10 -0500

    fixed peak-performance example

--- webpack.dev.js
+++ webpack.dev.js
@@ -38,7 +38,7 @@ const devPages = {
 }
 
 const devDir = './dist/html';
-const devPage = `${devPages.Button}.html`;
+const devPage = `${devPages.Backdrop}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
