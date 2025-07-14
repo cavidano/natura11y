8585367247d48b17bca79927fcb15d3a08788756
Carl Avidano <cavidano@gmail.com>
2025-07-14 09:56:36 -0400

    before js  bug fixes

--- webpack.dev.js
+++ webpack.dev.js
@@ -38,7 +38,7 @@ const devPages = {
 }
 
 const devDir = './dist/html';
-const devPage = `${devPages.Navigation}.html`;
+const devPage = `${devPages.Track}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
