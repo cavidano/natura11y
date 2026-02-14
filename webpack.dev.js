9759dc0444663d114c2b48822d102c1124cd47c0
Carl Avidano <cavidano@gmail.com>
2026-02-14 13:38:57 -0500

    fixed depreciated sass if() statement

--- webpack.dev.js
+++ webpack.dev.js
@@ -38,7 +38,7 @@ const devPages = {
 }
 
 const devDir = './dist/html';
-const devPage = `${devPages.Track}.html`;
+const devPage = `${devPages.Navigation}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
