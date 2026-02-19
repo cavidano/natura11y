c39202fd869f38b2e8efaa44f67170a08914ae4b
Carl Avidano <cavidano@gmail.com>
2026-02-19 06:25:06 -0500

    creating menu draft

--- webpack.dev.js
+++ webpack.dev.js
@@ -28,6 +28,7 @@ const devPages = {
     'Lightbox': 'lightbox',
     'Modal': 'modal',                   
     'Megamenu': 'megamenu',
+    'Mobilemenu': 'mobile-menu',
     'Navigation': 'navigation',
     'New': 'new-features',
     'Spacer': 'spacer',
@@ -38,7 +39,7 @@ const devPages = {
 }
 
 const devDir = './dist/html';
-const devPage = `${devPages.Track}.html`;
+const devPage = `${devPages.Mobilemenu}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
