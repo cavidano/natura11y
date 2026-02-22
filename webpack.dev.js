37f971afd35807d5da5d0b2e6bf01e81638b8758
Carl Avidano <cavidano@gmail.com>
2026-02-22 13:55:13 -0500

    updating motion media queries

--- webpack.dev.js
+++ webpack.dev.js
@@ -39,7 +39,7 @@ const devPages = {
 }
 
 const devDir = './dist/html';
-const devPage = `${devPages.Navigation}.html`;
+const devPage = `${devPages.Track}.html`;
 
 module.exports = merge(common, {
     mode: 'development',
