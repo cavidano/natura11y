a47ab653137bfb85abb5022225e5a03e9681f7bc
Carl Avidano <cavidano@gmail.com>
2026-03-06 11:01:14 -0500

    alert updates

--- src/js/alert.js
+++ src/js/alert.js
@@ -1,43 +1,24 @@
 import { delegateEvent } from './utilities/eventDelegation';
 
-export default class AlertDismissable {
-
-  // Private properties
-  #closeButtonHTML = `
-    <button class="button button--icon-only" aria-label="Close alert" aria-describedby="alert-description">
-        <span class="icon icon-close" aria-hidden="true"></span>
-    </button>
-  `;
+export default class Alert {
 
   // Private methods
+
   #handleAlertClose = (event) => {
     event.preventDefault();
-    const alertDismissable = event.target.closest('.alert--dismissable');
-    if (alertDismissable) {
-      alertDismissable.classList.add('dismissed');
-      alertDismissable.addEventListener('animationend', () => {
-        alertDismissable.remove();
+    const alert = event.target.closest('.alert');
+    if (alert) {
+      alert.classList.add('dismissed');
+      alert.addEventListener('animationend', () => {
+        alert.remove();
       });
     }
   };
 
-  #initializeAlert = (alertDismissable) => {
-    alertDismissable.insertAdjacentHTML('afterbegin', this.#closeButtonHTML);
-
-    // Add aria-live attribute for accessibility
-    alertDismissable.setAttribute('role', 'alert');
-    alertDismissable.setAttribute('aria-live', 'assertive');
-    alertDismissable.setAttribute('aria-atomic', 'true');
-  };
-
   // Public methods
-  init = () => {
-    // Initialize existing alerts
-    document.querySelectorAll('.alert--dismissable').forEach((alertDismissable) => {
-      this.#initializeAlert(alertDismissable);
-    });
 
-    // Delegate event for dynamically added alerts
-    delegateEvent(document, 'click', '.alert--dismissable .button--icon-only', this.#handleAlertClose);
+  init = () => {
+    delegateEvent(document, 'click', '[data-alert-close]', this.#handleAlertClose);
   };
+
 }
