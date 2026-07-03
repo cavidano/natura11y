7140667a7c99da99de35b2b5d3ae4ca72ef29c15
Carl Avidano <cavidano@gmail.com>
2026-07-03 06:21:24 -0400

    Updating backdrop examples

--- apps/storybook/stories/media.ts
+++ apps/storybook/stories/media.ts
@@ -1,8 +1,8 @@
 export const storyMedia = {
-  landscapeImage: '/story-assets/sample-landscape.jpg',
-  cardImage: '/story-assets/sample-landscape.jpg',
-  videoOne: '/story-assets/sample-video-1.mp4',
-  videoOneThumbnail: '/story-assets/sample-video-1-thumbnail.jpg',
-  videoTwo: '/story-assets/sample-video-2.mp4',
-  videoTwoThumbnail: '/story-assets/sample-video-2-thumbnail.jpg',
+  landscapeImage: '/story-assets/backdrop/landscape.jpg',
+  cardImage: '/story-assets/backdrop/landscape.jpg',
+  videoOne: '/story-assets/backdrop/video-1.mp4',
+  videoOneThumbnail: '/story-assets/backdrop/video-1-thumbnail.jpg',
+  videoTwo: '/story-assets/backdrop/video-2.mp4',
+  videoTwoThumbnail: '/story-assets/backdrop/video-2-thumbnail.jpg',
 };
