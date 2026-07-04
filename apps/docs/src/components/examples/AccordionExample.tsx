5c5572d9b0691043c99919fd74e2de85d4e837f1
Carl Avidano <cavidano@gmail.com>
2026-07-04 12:45:50 -0400

    themes

--- apps/docs/src/components/examples/AccordionExample.tsx
+++ apps/docs/src/components/examples/AccordionExample.tsx
@@ -3,28 +3,33 @@ import { Accordion, AccordionItem } from '@natura11y/react';
 interface AccordionExampleProps {
   defaultOpen?: string | null;
   headingLevel?: 2 | 3 | 4 | 5 | 6 | null;
+  idPrefix?: string;
 }
 
 const AccordionExample = ({
   defaultOpen = null,
   headingLevel = null,
+  idPrefix = '',
 }: AccordionExampleProps) => (
-  <Accordion defaultOpen={defaultOpen} headingLevel={headingLevel}>
-    <AccordionItem itemId='danaus-plexippus' title='Danaus Plexippus'>
+  <Accordion
+    defaultOpen={defaultOpen ? `${idPrefix}${defaultOpen}` : null}
+    headingLevel={headingLevel}
+  >
+    <AccordionItem itemId={`${idPrefix}danaus-plexippus`} title='Danaus Plexippus'>
       <p>
         The monarch butterfly is one of the most familiar butterflies in North America.
         Its orange and black pattern makes it instantly recognizable.
       </p>
     </AccordionItem>
 
-    <AccordionItem itemId='papilio-glaucus' title='Papilio Glaucus'>
+    <AccordionItem itemId={`${idPrefix}papilio-glaucus`} title='Papilio Glaucus'>
       <p>
         The eastern tiger swallowtail is a large butterfly known for yellow wings,
         black tiger-like stripes, and graceful flight.
       </p>
     </AccordionItem>
 
-    <AccordionItem itemId='vanessa-cardui' title='Vanessa Cardui'>
+    <AccordionItem itemId={`${idPrefix}vanessa-cardui`} title='Vanessa Cardui'>
       <p>
         The painted lady is widely distributed and highly migratory, appearing across
         many regions and habitats throughout the year.
