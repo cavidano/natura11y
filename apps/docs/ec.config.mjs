ba849e8a4c2ae2f96907dabc42c648a348ac3520
Carl Avidano <cavidano@gmail.com>
2026-07-04 12:52:05 -0400

    Update ec.config.mjs

--- apps/docs/ec.config.mjs
+++ apps/docs/ec.config.mjs
@@ -3,16 +3,16 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
 
 export default defineEcConfig({
   plugins: [pluginLineNumbers()],
-  themes: ['github-light', 'github-dark'],
+  themes: ['github-light-high-contrast', 'github-dark-high-contrast'],
   defaultProps: {
     showLineNumbers: false,
   },
   customizeTheme(theme) {
-    if (theme.name === 'github-light') {
+    if (theme.name === 'github-light-high-contrast') {
       theme.name = 'light';
     }
 
-    if (theme.name === 'github-dark') {
+    if (theme.name === 'github-dark-high-contrast') {
       theme.name = 'dark';
     }
   },
