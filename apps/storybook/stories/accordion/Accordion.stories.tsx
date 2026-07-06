d08758b509f41e1d46c8660957c9dad229efcea5
Carl Avidano <cavidano@gmail.com>
2026-07-06 10:07:53 -0400

    cleaned up accordion example

--- apps/storybook/stories/accordion/Accordion.stories.tsx
+++ apps/storybook/stories/accordion/Accordion.stories.tsx
@@ -82,11 +82,49 @@ const itemIds = items.map(({ itemId }) => itemId);
 
 const accordionReactCode = `<Accordion>
   <Accordion.Item itemId="danaus-plexippus" title="Danaus Plexippus">
-    <p>The monarch butterfly is a milkweed butterfly and one of the most familiar North American pollinators.</p>
+    <p>
+      The monarch butterfly or simply monarch is a milkweed butterfly in the
+      family Nymphalidae. Other common names, depending on region, include
+      milkweed, common tiger, wanderer, and black veined brown. It may be
+      the most familiar <a href="#1">North American</a> butterfly, and is
+      considered an iconic pollinator species.
+    </p>
   </Accordion.Item>
 
   <Accordion.Item itemId="papilio-polyxenes" title="Papilio Polyxenes">
-    <p>The black swallowtail is a butterfly found throughout much of North America.</p>
+    <p>
+      The black swallowtail, American swallowtail, or parsnip swallowtail,
+      is a butterfly found throughout much of <a href="#1">North America</a>.
+      It is the state butterfly of Oklahoma and New Jersey.
+    </p>
+  </Accordion.Item>
+
+  <Accordion.Item itemId="hyalophora-cecropia" title="Hyalophora Cecropia">
+    <p>
+      The cecropia moth is <a href="#1">North America's</a> largest native
+      moth. It is a member of the family Saturniidae, or giant silk moths.
+      Females have been documented with a wingspan of five to seven inches
+      or more.
+    </p>
+  </Accordion.Item>
+
+  <Accordion.Item itemId="deilephila-elpenor" title="Deilephila Elpenor">
+    <p>
+      The elephant hawk moth or large elephant hawk moth, is a moth in the
+      family Sphingidae. Its common name is derived from the caterpillar's
+      resemblance to an elephant's trunk. It is most common in
+      <a href="#1">central Europe</a> and is distributed throughout the
+      Palearctic region.
+    </p>
+  </Accordion.Item>
+
+  <Accordion.Item itemId="papilio-troilus" title="Papilio Troilus">
+    <p>
+      The spicebush swallowtail or green-clouded butterfly, is a common
+      black swallowtail butterfly found in <a href="#1">North America</a>.
+      It has two subspecies, Papilio troilus troilus and Papilio troilus
+      ilioneus, the latter found mainly in the Florida peninsula.
+    </p>
   </Accordion.Item>
 </Accordion>`;
 
